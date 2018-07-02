f64bb4416e42eac69b46998d22deaa389cb41eac
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-07-02 18:16:44 +0300

    added tab placeholders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopMenuComponent
+    TopMenuComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule
