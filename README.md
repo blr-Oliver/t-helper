6187d60112f87cc10102c32f54a21506d4008d2b
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-07-02 17:12:29 +0300

    added bootstrap

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NgStarter
+# Tournament helper
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,10 +13,11 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  /*
+  it(`should have as title 'Tournament helper'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Tournament helper');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -24,4 +25,5 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-starter!');
   }));
+  */
 });
