c6291ab1717d63b87ac8fa19db26553528700897
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-07-03 11:28:37 +0300

    added components for more tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { PlayersComponent } from './players/players.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { DuelsComponent } from './duels/duels.component';
+import { ScoresComponent } from './scores/scores.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopMenuComponent,
     TabsComponent,
-    PlayersComponent
+    PlayersComponent,
+    ScheduleComponent,
+    DuelsComponent,
+    ScoresComponent
   ],
   imports: [
     BrowserModule
