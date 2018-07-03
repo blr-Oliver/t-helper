23bdb90045565598e5554b937650a31b1bd8e9a2
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-07-03 11:23:59 +0300

    added "Players" tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { TabsComponent } from './tabs/tabs.component';
+import { PlayersComponent } from './players/players.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopMenuComponent,
-    TabsComponent
+    TabsComponent,
+    PlayersComponent
   ],
   imports: [
     BrowserModule
