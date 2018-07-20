7934037e403adc5b8f693b31f1b31369f2960b8a
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-07-20 13:14:55 +0300

    implemented tab for standings

--- src/app/scores/scores.component.ts
+++ src/app/scores/scores.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Standings} from '../model/Standings';
 
 @Component({
   selector: '[data-scores]',
@@ -6,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scores.component.scss']
 })
 export class ScoresComponent implements OnInit {
-
-  constructor() { }
+  @Input() standings: Standings;
 
   ngOnInit() {
   }
