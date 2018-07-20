a7ae3bb87fb66056e66645f5c86a6b2b52b9aa6e
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-07-20 19:11:27 +0300

    Player's name initialized by his id

--- src/app/model/Player.ts
+++ src/app/model/Player.ts
@@ -2,7 +2,8 @@ export class Player {
   readonly id: string;
   name: string;
 
-  constructor(id: string) {
+  constructor(id: string, name?: string) {
     this.id = id;
+    this.name = name || id;
   }
 }
