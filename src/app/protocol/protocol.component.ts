5f7fa31a8f92afaf98f1c73d2e867ed502cfa9b1
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-08-13 15:38:05 +0300

    simplified observable handling

--- src/app/protocol/protocol.component.ts
+++ src/app/protocol/protocol.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, zip} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
 import {TournamentService} from '../service/tournament.service';
-import {map, mergeMap} from 'rxjs/operators';
+import {map, mergeMap, tap} from 'rxjs/operators';
 import {GameEntity} from '../model/entity/GameEntity';
 
 @Component({
@@ -10,7 +10,7 @@ import {GameEntity} from '../model/entity/GameEntity';
 })
 export class ProtocolComponent implements OnInit {
   protocol$: Observable<GameEntity>;
-  private tournamentId: number;
+  gameId: string;
 
   constructor(
     private tournamentService: TournamentService,
@@ -18,15 +18,13 @@ export class ProtocolComponent implements OnInit {
   }
 
   ngOnInit() {
-    const self = this;
-    this.route.parent.paramMap.subscribe(params => this.tournamentId = +params.get('id'));
-    this.protocol$ = this.route.paramMap.pipe(
-      mergeMap(function (params) {
-        const tour = +params.get('tour'), table = +params.get('table');
-        return self.tournamentService.get(self.tournamentId).pipe(
-          map(t => t.games[tour - 1][table - 1])
-        );
-      })
+    this.protocol$ = zip(this.route.parent.paramMap, this.route.paramMap).pipe(
+      tap((twoMaps) => this.tournamentService.get(twoMaps[0].get('id'))),
+      map(twoMaps => twoMaps[1]),
+      tap(params => this.gameId = `game-${params.get('tour')}-${params.get('table')}`),
+      mergeMap(params => this.tournamentService.getCurrent().pipe(
+        map(t => t.games[+params.get('tour') - 1][+params.get('table') - 1]))
+      )
     );
   }
 }
