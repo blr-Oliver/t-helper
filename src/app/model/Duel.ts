9c879c2e7079c565d48bfe8a9231fc5354b7c836
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-08-13 17:02:05 +0300

    switched to wrappers

--- src/app/model/Duel.ts
+++ src/app/model/Duel.ts
@@ -1,28 +1,28 @@
 import {PairPosition} from './PairPosition';
-import {Game} from './Game';
-import {Pair} from './Pair';
 import {MaxTournamentScoring} from './TournamentScoring';
+import {GameEntity} from './entity/GameEntity';
+import {PairEntity} from './entity/PairEntity';
 
 export class Duel {
   readonly deal: number;
   readonly position: PairPosition;
-  readonly games: [Game, Game];
-  readonly pairs: [Pair, Pair];
+  readonly games: [GameEntity, GameEntity];
+  readonly pairs: [PairEntity, PairEntity];
   readonly scores: DuelScores;
 
-  constructor(game1: Game, game2: Game, position: PairPosition) {
+  constructor(game1: GameEntity, game2: GameEntity, position: PairPosition) {
     if (game1.deal !== game2.deal) {
       throw new Error('Illegal duel initialization');
     }
     this.deal = game1.deal;
     this.position = position;
-    this.games = <[Game, Game]> [game1, game2]
-      .sort((a, b) => a.pairs[position].players[0].id.localeCompare(b.pairs[position].players[0].id));
-    this.pairs = <[Pair, Pair]> this.games.map(p => p.pairs[position]);
+    this.games = <[GameEntity, GameEntity]> [game1, game2]
+      .sort((a, b) => a.pairs[position].name.localeCompare(b.pairs[position].name));
+    this.pairs = <[PairEntity, PairEntity]> this.games.map(p => p.pairs[position]);
     this.scores = new DuelScores(this);
   }
 
-  getPairIndex(pair: Pair): number {
+  getPairIndex(pair: PairEntity): number {
     if (this.pairs[0] === pair) { return 0; }
     if (this.pairs[1] === pair) { return 1; }
     return null;
