e4c07775f00fad95ffa3bb350d5d241bf9026aba
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-08-14 05:18:43 +0300

    singleton UpdateManager - shared entry point for update events

---
+++ src/app/service/UpdateManager.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {ConnectableObservable, Observable, Subject} from 'rxjs';
+import {UpdateEvent} from './UpdateEvent';
+import {multicast, tap} from 'rxjs/operators';
+
+@Injectable()
+export class UpdateManager {
+  private readonly subject: Subject<UpdateEvent>;
+  private readonly stream: ConnectableObservable<UpdateEvent>;
+
+  constructor() {
+    this.subject = new Subject<UpdateEvent>();
+    this.stream = <ConnectableObservable<UpdateEvent>> this.subject.pipe(multicast(new Subject<UpdateEvent>()));
+    this.stream.connect();
+  }
+
+  registerUpdate(event: UpdateEvent) {
+    this.subject.next(event);
+  }
+
+  getStream(): Observable<UpdateEvent> {
+    return this.stream.pipe(
+      tap(update => console.log(update))
+    );
+  }
+}
