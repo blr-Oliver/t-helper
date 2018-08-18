f0aa8a96d9eba876a3fd7c3a865f052e139b8a2f
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-08-18 03:18:42 +0300

    added seating mapping

--- src/main/java/com/oliver/thelper/model/GameSlot.java
+++ src/main/java/com/oliver/thelper/model/GameSlot.java
@@ -1,4 +1,5 @@
 package com.oliver.thelper.model;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 public class GameSlot {
@@ -9,7 +10,7 @@ public class GameSlot {
   private short deal;
   @JsonInclude(JsonInclude.Include.NON_NULL)
   private Position dealer;
-  private Seating<String> players;
+  private Seating<String> players = new Seating<>();
   
   public int getId() { return id; }
   public int getSid() { return sid; }
@@ -18,4 +19,14 @@ public class GameSlot {
   public short getDeal() { return deal; }
   public Position getDealer() { return dealer; }
   public Seating<String> getPlayers() { return players; }
+  
+  @JsonIgnore public String getPlayerN() { return this.players.get(Position.N); }
+  @JsonIgnore public String getPlayerE() { return this.players.get(Position.E); }
+  @JsonIgnore public String getPlayerS() { return this.players.get(Position.S); }
+  @JsonIgnore public String getPlayerW() { return this.players.get(Position.W); }
+
+  public void setPlayerN(String value) { this.players.put(Position.N, value); }
+  public void setPlayerE(String value) { this.players.put(Position.E, value); }
+  public void setPlayerS(String value) { this.players.put(Position.S, value); }
+  public void setPlayerW(String value) { this.players.put(Position.W, value); }
 }
