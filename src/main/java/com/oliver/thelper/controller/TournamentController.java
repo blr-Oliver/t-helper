19232c887818f76792eaab045de17e73b0438fef
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-08-19 16:46:39 +0300

    removed unused indirect protocol endpoint

--- src/main/java/com/oliver/thelper/controller/TournamentController.java
+++ src/main/java/com/oliver/thelper/controller/TournamentController.java
@@ -3,28 +3,20 @@ package com.oliver.thelper.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.oliver.thelper.annotation.Views;
-import com.oliver.thelper.model.GameSlot;
-import com.oliver.thelper.model.Protocol;
 import com.oliver.thelper.model.Tournament;
-import com.oliver.thelper.repository.GameSlotRepository;
-import com.oliver.thelper.repository.ProtocolRepository;
 import com.oliver.thelper.repository.TournamentRepository;
 
 @RestController
 @RequestMapping("/api/tournaments")
 public class TournamentController {
   @Autowired private TournamentRepository tournamentRepo;
-  @Autowired private GameSlotRepository gameSlotRepo;
-  @Autowired private ProtocolRepository protocolRepo;
 
   @JsonView(Views.ShortView.class)
   @RequestMapping(method = RequestMethod.GET)
@@ -35,25 +27,5 @@ public class TournamentController {
   @RequestMapping(value = "/{id}", method = RequestMethod.GET)
   public Tournament getOne(@PathVariable Integer id) {
     return this.tournamentRepo.findById(id).get();
-  }
-
-  @RequestMapping(value = "/{tid}/games/{tour}/{table}", method = RequestMethod.GET)
-  public Protocol getProtocol(@PathVariable Integer tid, @PathVariable Short tour, @PathVariable Short table) {
-    Tournament tournament = this.tournamentRepo.findById(tid).get();
-    GameSlot game = gameSlotRepo.findInScheduleByTourAndTable(tournament.getSid(), tour, table).get();
-    return protocolRepo.findByTournamentAndGameSlot(tid, game.getId()).get();
-  }
-
-  @RequestMapping(value = "/{tid}/games/{tour}/{table}", method = RequestMethod.PUT)
-  public ResponseEntity<?> saveProtocol(@PathVariable Integer tid, @PathVariable Short tour, @PathVariable Short table, @RequestBody Protocol protocol) {
-    Protocol saved = protocolRepo.findByTournamentAndGameSlot(tid, protocol.getGid()).get();
-
-    saved.setLevel(protocol.getLevel());
-    saved.setSuit(protocol.getSuit());
-    saved.setOwner(protocol.getOwner());
-    saved.setTricks(protocol.getTricks());
-
-    protocolRepo.save(saved);
-    return ResponseEntity.noContent().build();
   }
 }
