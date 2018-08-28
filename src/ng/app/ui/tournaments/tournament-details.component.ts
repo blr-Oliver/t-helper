286ac761b0b5acf662b38b7655b9b5f484be2723
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-08-28 19:09:43 +0300

    debounce delay at changes

--- src/ng/app/ui/tournaments/tournament-details.component.ts
+++ src/ng/app/ui/tournaments/tournament-details.component.ts
@@ -1,16 +1,22 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, Subject} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
 import {TournamentService} from '../../service/tournament.service';
-import {mergeMap} from 'rxjs/operators';
+import {debounceTime, distinctUntilKeyChanged, mergeMap, skipWhile, tap} from 'rxjs/operators';
 import {Tournament} from '../../model/Tournament';
 import {UpdateManager} from '../../service/UpdateManager';
+import {UpdateEvent} from '../../service/UpdateEvent';
+import {TournamentDTO} from '../../model/dto/TournamentDTO';
 
 @Component({
   templateUrl: './tournament-details.component.html'
 })
 export class TournamentDetailsComponent implements OnInit {
+  private static readonly DEBOUNCE_PROPERTIES: string[] = ['name', 'description'];
+  private static readonly DEBOUNCE_DELAY = 600;
+
   tournament$: Observable<Tournament>;
+  debounceBarriers: { [property: string]: Subject<UpdateEvent> };
 
   constructor(
     private tournamentService: TournamentService,
@@ -19,18 +25,32 @@ export class TournamentDetailsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.tournament$ =
-      this.route.paramMap.pipe(
-        mergeMap(p => this.tournamentService.get(p.get('id')))
-      );
+    this.tournament$ = this.route.paramMap.pipe(
+      mergeMap(p => this.tournamentService.get(p.get('id'))),
+      tap(tournament => this.initBarriers(tournament.data))
+    );
   }
 
   onUpdate(subject: Tournament, property: string, newValue: any) {
-    this.updateManager.registerUpdate({
+    this.debounceBarriers[property].next({
       type: 'tournament',
       subject: subject.data,
       property: property,
       currentValue: newValue
     });
+  }
+
+  private initBarriers(tournament: TournamentDTO): void {
+    const handler = (event: UpdateEvent) => this.updateManager.registerUpdate(event);
+    this.debounceBarriers = TournamentDetailsComponent.DEBOUNCE_PROPERTIES.reduce((hash, property) => {
+      const initial = tournament[property];
+      hash[property] = new Subject<UpdateEvent>().pipe(
+        debounceTime(TournamentDetailsComponent.DEBOUNCE_DELAY),
+        distinctUntilKeyChanged('currentValue'),
+        skipWhile(event => event.currentValue === initial)
+      );
+      hash[property].subscribe(handler);
+      return hash;
+    }, {});
   }
 }
