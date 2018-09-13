4a51033b4528f6654a22930d082be6cf021add2a
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-09-13 17:44:37 +0300

    fixed layout inconsistency due to intermediate component elements

--- src/ng/app/ui/print/protocol-player.component.ts
+++ src/ng/app/ui/print/protocol-player.component.ts
@@ -1,10 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Player} from '../../model/Player';
-import {classAlias, Position} from '../../model/Position';
+import {Position} from '../../model/Position';
 import {Game} from '../../model/Game';
 
 @Component({
-  selector: 'protocol-player',
+  selector: '.player-seat',
   templateUrl: './protocol-player.component.html'
 })
 
@@ -12,12 +12,10 @@ export class ProtocolPlayerComponent implements OnInit {
   @Input() game: Game;
   @Input() position: Position;
 
-  positionClass: string;
   player: Player;
   dealer: boolean;
 
   ngOnInit() {
-    this.positionClass = classAlias(this.position);
     this.player = this.game.players[this.position];
     this.dealer = this.game.gameSlot.dealer === this.position;
   }
