209d5006dad058ba7928a36cb4f944cee1f338ab
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-10-30 19:26:01 +0300

    explicit input list to the printer

--- src/ng/app/ui/print/protocol-printer.component.ts
+++ src/ng/app/ui/print/protocol-printer.component.ts
@@ -1,28 +1,13 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Component, Input, ViewEncapsulation} from '@angular/core';
 import {Game} from '../../model/Game';
-import {ActivatedRoute} from '@angular/router';
-import {map, mergeMap} from 'rxjs/operators';
-import {TournamentService} from '../../service/tournament.service';
 
 @Component({
+  selector: 'protocol-printer',
   templateUrl: './protocol-printer.component.html',
   styleUrls: ['./protocol-printer.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ProtocolPrinterComponent implements OnInit {
-  games$: Observable<Game[]>;
-
-  constructor(
-    private tournamentService: TournamentService,
-    private route: ActivatedRoute) {
-  }
-
-  ngOnInit(): void {
-    this.games$ = this.route.parent.paramMap.pipe(
-      mergeMap(params => this.tournamentService.get(params.get('id'))),
-      map(t => t.games),
-      map(games => [].concat(...games))
-    );
-  }
+export class ProtocolPrinterComponent {
+  @Input() games: Game[];
+  @Input() blanks: number;
 }
