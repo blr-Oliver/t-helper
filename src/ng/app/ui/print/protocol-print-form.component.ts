00535d2bae9ec393adcaf644eb5ef972c2f12bf9
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-10-31 16:27:57 +0300

    styles for printable middle block separated

--- src/ng/app/ui/print/protocol-print-form.component.ts
+++ src/ng/app/ui/print/protocol-print-form.component.ts
@@ -4,6 +4,7 @@ import {Game} from '../../model/Game';
 @Component({
   selector: 'protocol-print-form',
   templateUrl: './protocol-print-form.component.html',
+  styleUrls: ['./protocol-print-form.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class ProtocolPrintFormComponent {
